129ad478a799fd09d01f8157b632c7bbd0c00edd
serge-sans-paille <serge.guelton@telecom-bretagne.eu>
2019-04-30 22:33:49 +0200

    New benchmark: l1norm

---
+++ benchmarks/l1norm.py
@@ -0,0 +1,8 @@
+#from: https://stackoverflow.com/questions/55854611/efficient-way-of-vectorizing-distance-calculation/55877642#55877642
+#setup: import numpy as np ; N = 80; x = np.random.rand(N,N); y = np.random.rand(N,N)
+#run: l1norm(x, y)
+
+#pythran export l1norm(float64[][], float64[:,:])
+import numpy as np
+def l1norm(x, y):
+    return np.sum(np.abs(x[:, None, :] - y), axis=-1)
