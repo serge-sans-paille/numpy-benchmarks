0cb6635f32a1cd39e60344f6117b39bf820bcc73
serge-sans-paille <serge.guelton@telecom-bretagne.eu>
2019-08-20 23:09:23 +0200

    Normalize/Delete some benchmarks
    Remove all benchmarks involving only explicit looping. These benchmarks are
    unfair to Python, as they only exhibit the well-known fact that iterating over
    numpy array is slow.

--- benchmarks/allpairs_distances.py
+++
@@ -1,8 +0,0 @@
-#setup: import numpy as np ; np.random.seed(0); N = 500 ; X, Y = np.random.randn(100,N), np.random.randn(40,N)
-#run: allpairs_distances(X, Y)
-
-#pythran export allpairs_distances(float64[][], float64[][])
-import numpy as np
-
-def allpairs_distances(X,Y):
-  return np.array([[np.sum( (x-y) ** 2) for x in X] for y in Y])
